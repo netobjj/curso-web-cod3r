ab06efabaeb67f78b0b92aa518db783e251ffd59
netobjj <joseeuclides18@gmail.com>
2025-07-04 23:41:12 -0300

    add object revisao

--- objeto/objetoRevisao.js
+++ objeto/objetoRevisao.js
@@ -9,3 +9,26 @@ delete produto['valor do produto']
 
 console.log(produto);
 
+const carro = {
+    nome: "Doblo",
+    valor: 90000,
+    proprietario: {
+        nome: "Neto",
+        idade: 90,
+        endereco:{
+           logradouro: "Rua 7/09",
+           numero: 992
+        }
+        
+    },
+    condutores: [{
+        //...
+    },],
+    calcularValorDoSeguro: function() {
+        //....
+    }
+}
+
+
+carro.valor = 1000
+console.log(carro)
